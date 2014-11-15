bbb97eb8617e16e0ef3ea8785d693969fc54c954
Beth Hadley <bhadley@mit.edu>
2014-11-14 22:12:00 -0500

    able to send a text message now

---
+++ helloworldppat/SendTextViewController.m
@@ -0,0 +1,54 @@
+//
+//  ThirdViewController.m
+//  helloworldppat
+//
+//  Created by amadeus on 10/6/14.
+//  Copyright (c) 2014 amadeus. All rights reserved.
+//
+
+#import "SendTextViewController.h"
+
+#import "utils.h"
+@interface SendTextViewController ()
+
+@end
+
+@implementation SendTextViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+- (IBAction)sendTextMsgRequest:(id)sender {
+    NSLog(@"hi!!!");
+  
+    [self requestForUserID:@"Margaret" withRequest:  _requestTxtMsg.text];
+   
+}
+ 
+
+
+@end
