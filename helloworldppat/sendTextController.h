279cb2259f0b3f40c43079ac94300d6f64d31cf2
Beth Hadley <bhadley@mit.edu>
2014-11-14 21:23:27 -0500

    blah

---
+++ helloworldppat/sendTextController.h
@@ -0,0 +1,14 @@
+//
+//  ThirdViewController.h
+//  helloworldppat
+//
+//  Created by amadeus on 10/6/14.
+//  Copyright (c) 2014 amadeus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sendTextController : UIViewController
+
+- (IBAction)sendTextMsgRequest:(id)sender;
+@end
